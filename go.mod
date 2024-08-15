dbf28aee61da55a3e0eb086ac73def7b2c430bd7
Chaitanya Khajuria <cWX1351795@china.huawei.com>
2024-08-15 10:55:00 +0300

    Doggo introduced

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ChaitanyaKhajuria/puppy
 
 go 1.22.6
+
+require github.com/ChaitanyaKhajuria/doggo v0.0.0-20240815073157-a4a74e0ad031 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/ChaitanyaKhajuria/doggo"
+)
+
 func Bark() string {
-	return "Woof"
+	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return doggo.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return doggo.WhenGrownUp(Barks())
 }
